4de64ed3797adda76eae8c3e42db9cfaff51d21d
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-03-08 20:42:32 -0300

    Aula 08/03 - React-Native

--- src/Ex_03/index.js
+++ src/Ex_03/index.js
@@ -1,10 +1,42 @@
+import React, {useState} from "react";
 import {View, Text, TouchableOpacity} from "react-native";
 import styles from "./style";
 
 export default function Ex_03 () {
+
+    const [numero, setNumero] = useState(0);
+
+    function ola(mundo){
+        alert("Olá " + mundo);
+    }
+
+    function incremento(){
+        const inc = numero +1;
+        setNumero(inc);
+    }
+
     return (
         <View style = {styles.container}>
-            <Text>Exemplo 3</Text>
+            <Text style = {styles.titulo}>
+                Exemplo 3
+            </Text>
+            <TouchableOpacity 
+            style = {styles.botao}
+            onPress={() => {ola ("Mundo")}}
+            >
+
+                <Text style = {styles.txtbotao}>Diga "Olá Mundo"</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.titulo}>Valor: {numero}</Text>
+
+            <TouchableOpacity 
+            style = {styles.botao}
+            onPress={() => incremento()}
+            >
+
+            <Text style = {styles.txtbotao}>+ 1"</Text>
+            </TouchableOpacity>
         </View>
     );
 }
