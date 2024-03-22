9722b8d60d4ebd2c5ba7a61e56f0024fa09260d4
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-03-22 20:39:55 -0300

    Aula 22-03 (Use.State)

--- src/Ex_04/index.js
+++ src/Ex_04/index.js
@@ -1,12 +1,39 @@
-import { View, Text} from "react-native";
+import { View, Text, TextInput, Pressable} from "react-native";
 import styles from "./styles";
+import {useState} from "react";
+
+
+
 
 export default function Ex_04(){
+
+    const [mensagem, setMensagem] = useState("Sua mensagem aqui");
+    const [mensagem2, setMensagem2] = useState("Outra mensagem");
+    const [mensagemTela, setMensagemTela] = useState("");    
+
     return(
         <View style={styles.container}>
-            <Text style={styles.titulo}>
-                Exemplo 4
-            </Text>
+            <Text style={styles.titulo}>Exemplo 4</Text>
+
+            <Text style={styles.txt_saida}>{mensagem}</Text>
+
+            <TextInput 
+            style={styles.txt_entrada}
+            placeholder="Digite o texto aqui..."
+            onChangeText={(txt) => setMensagem(txt)}
+            />
+
+            <Text style={styles.txt_saida}>{mensagemTela}</Text>
+
+            <TextInput
+            style={styles.txt_entrada}
+            // placeholder="Digite a outra mensagem..."
+            onChangeText={(entrada) => setMensagem2(entrada)}
+            />
+
+            <Pressable onPress={()=> setMensagemTela(mensagem2)} onChangeText={(mensagem2) => (mensagem2)}>
+                <Text>Exibir Mensagem</Text>
+            </Pressable>
         </View>
     );
 }
--- src/Ex_04/styles.js
+++ src/Ex_04/styles.js
@@ -14,6 +14,21 @@ const styles = StyleSheet.create({
         fontWeight:"bold",
         color:"#b71c1c",
     },
+    txt_saida: {
+        margin:20,
+        fontSize:25,
+        color:"#e53935"
+    },
+    txt_entrada:{
+        borderWidth:3,
+        textAlign:"center",
+        fontSize:22,
+        borderColor:"#b71c1c",
+        height:40,
+        width:"70%",
+        color:"#e53935",
+        borderRadius:10,
+    },
 })
 
 export default styles;
