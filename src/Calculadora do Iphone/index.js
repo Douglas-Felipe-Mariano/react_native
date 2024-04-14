a4e4414eeb74c7cc91bbac1ee04a584063e0f5a8
Douglas-Felipe-Mariano <douglas.mariano01@etec.sp.gov.br>
2024-04-14 09:33:37 -0300

    Estilização da calculadorinha

--- src/Calculadora do Iphone/index.js
+++ src/Calculadora do Iphone/index.js
@@ -1,26 +1,58 @@
-import {View, Text, Pressable} from "react-native"
+import { View, Text, Pressable } from "react-native"
 import { useState } from "react";
 import styles from "./styles";
+import { TextInput } from "react-native-web";
 
-export default function calculadora(){
-    return(
+export default function calculadora() {
+
+    const [n1, setN1] = useState(0)
+    const [n2, setN2] = useState(0)
+    const [n3, setN3] = useState(0)
+    const [n4, setN4] = useState(0)
+    const [n5, setN5] = useState(0)
+    const [n6, setN6] = useState(0)
+    const [n7, setN7] = useState(0)
+    const [total, setTotal] = useState(0)
+
+    return (
         <View style={styles.view_pai}>
-            <View style={styles.view_botoes}>
-                <Pressable style={styles.botoes_acao}><Text>AC</Text></Pressable>
-                <Pressable style={styles.botoes_acao}><Text>%</Text></Pressable>
-                <Pressable style={styles.botoes_acao}><Text>=</Text></Pressable>
-                <Pressable style={styles.botoes_acao}><Text>off</Text></Pressable>
-            </View>
-            <View>
+            <View style={styles.view_calculadora}>
 
-            </View>
+                <TextInput ></TextInput>
 
-            <View>
+                <View style={styles.view_botoes}>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>C</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>+/-</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>%</Text></Pressable>
+                    <Pressable style={styles.botao_amarelo}><Text style={styles.txt_branco}>÷</Text></Pressable>
+                </View>
 
-            </View>
+                <View style={styles.view_botoes}>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>7</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>8</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>9</Text></Pressable>
+                    <Pressable style={styles.botao_amarelo}><Text style={styles.txt_branco}>X</Text></Pressable>
+                </View>
 
-            <View>
+                <View style={styles.view_botoes}>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>4</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>5</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>6</Text></Pressable>
+                    <Pressable style={styles.botao_amarelo}><Text style={styles.txt_branco}>-</Text></Pressable>
+                </View>
 
+                <View style={styles.view_botoes}>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>1</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>2</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>3</Text></Pressable>
+                    <Pressable style={styles.botao_amarelo}><Text style={styles.txt_branco}>+</Text></Pressable>
+                </View>
+
+                <View style={styles.view_botoes}>
+                    <Pressable style={styles.botao_zero}><Text style={styles.txt_preto}>0</Text></Pressable>
+                    <Pressable style={styles.botoes_acao}><Text style={styles.txt_preto}>,</Text></Pressable>
+                    <Pressable style={styles.botao_amarelo}><Text style={styles.txt_preto}>=</Text></Pressable>
+                </View>
             </View>
         </View>
     );
--- src/Calculadora do Iphone/styles.js
+++ src/Calculadora do Iphone/styles.js
@@ -9,16 +9,58 @@ const styles = StyleSheet.create({
         alignItems:"center"
     },
     view_botoes:{
+
         flexDirection:"row",
-        justifyContent:"space-between",
+        justifyContent:"center",
         width:"%100",   
+    },
+    view_calculadora:{
+        flex:1,
+        width:"30%",
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor: "#000",
+        borderRadius:25
     },
     botoes_acao:{
         backgroundColor:'#d9d9d9',
-        width:"25%",
+        width:50,
+        height:50,
         alignItems:'center',
         margin:15,
-        borderRadius:15
+        borderRadius:"100%",
+        padding:8,
+        justifyContent:"center"
+    },
+    txt_preto:{
+        color:"000",
+        fontSize:20 ,
+        fontWeight:"bold"
+    },
+    botao_amarelo:{
+        backgroundColor:"#ffa500",
+        width:50,
+        height:50,
+        alignItems:'center',
+        margin:15,
+        borderRadius:"100%",
+        padding:8,
+        justifyContent:"center"
+    },
+    txt_branco:{
+        color:"#fff",
+        fontSize:20,
+        fontWeight:"bold"
+    },
+    botao_zero:{
+        backgroundColor:'#d9d9d9',
+        width:"60%",
+        height:"80%",
+        alignItems:'flex-start',
+        margin:15,
+        borderRadius:20,
+        padding:8,
+        justifyContent:"center"
     },
 })
 
